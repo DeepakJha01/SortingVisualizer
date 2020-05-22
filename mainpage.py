0dec4a272c39dce20c17da3265dcdb7de55ac9ab
Deepak Jha <45568398+DeepakJha01@users.noreply.github.com>
2020-05-22 19:10:05 +0530

    added quicksort
    updated wrt quickSort,mergeSort,bubbleSort,selectionSort

--- mainpage.py
+++ mainpage.py
@@ -6,9 +6,10 @@ import random
 from tkinter import *
 from tkinter import ttk
 from mergesort import mergeSort
+from quicksort import quickSort
 from bubblesort import bubbleSort
 from selectionsort import selectionSort
-
+from tkinter.scrolledtext import ScrolledText
 #---end import section---------------------
 
 
@@ -20,7 +21,7 @@ root.maxsize(root_width,root_height)   #(width,height)
 root.config(bg='black')
 
 #----GLOBAL VARIABLES---------
-allAlgos = ['Selection Sort','Bubble Sort','Merge Sort']
+allAlgos = ('Bubble Sort','Merge Sort','Quick Sort','Selection Sort')
 selectedAlgo = StringVar()
 pauseBool = False
 arr = []
@@ -90,6 +91,9 @@ def startSort():
     elif algoCombo.get() == 'Merge Sort':
         mergeSort(arr,displayArray,sortSpeed.get(),pauseBool,0,len(arr)-1)
 
+    elif algoCombo.get() == 'Quick Sort':
+        quickSort(arr,displayArray,sortSpeed.get(),pauseBool,0,len(arr)-1)
+
 # def pauseSort():
 #     global pauseBool
 #     pauseBool = True
@@ -107,12 +111,12 @@ outputCanvas = Canvas(root,height = 400,width = 950,bg = '#99ffff')
 outputCanvas.grid(row = 1,column = 0,padx = 10,pady = 10)
 
 #--input frame-------------------------------------------------------
-head = Label(inputFrame,text = 'Select Algorithm : ',fg = 'black',bg = '#ffff00',height = 1,width = 15,font = ('Comic Sans MS',14))
+head = Label(inputFrame,text = 'Select Algorithm -> ',fg = 'black',bg = '#ffff00',height = 1,width = 15,font = ('Comic Sans MS',14))
 head.grid(row = 0,column = 0,padx = 5,pady = 5)
 
-algoCombo = ttk.Combobox(inputFrame,textvariable = selectedAlgo, values = allAlgos,height = 1,width = 15,font = ('Comic Sans MS',14))
+algoCombo = ttk.Combobox(inputFrame,values = allAlgos,width = 15,font = ('Comic Sans MS',14))
 algoCombo.grid(row = 0,column = 1,padx = 5,pady = 5)
-algoCombo.current(0)
+algoCombo.current()
 
 generate = Button(inputFrame,text = 'Generate',fg = 'black',bg = '#ff0000',height = 1,width = 10,font = ('Comic Sans MS',14),command = generateRandomArray )
 generate.grid(row = 0,column = 2,padx = 5,pady = 5)
@@ -120,14 +124,17 @@ generate.grid(row = 0,column = 2,padx = 5,pady = 5)
 dataSize = Scale(inputFrame,from_ = 3,to = 100,resolution = 1,length = 400,width = 15,orient = HORIZONTAL,label = 'Data Size [n]',font = ('Comic Sans MS',10))
 dataSize.grid(row = 1,column = 0,padx = 5,pady = 5,columnspan = 2)
 
-play = Button(inputFrame,text = 'Play',fg = 'black',bg = '#00ff00',height = 1,width = 10,font = ('Comic Sans MS',14),command = startSort )
-play.grid(row = 1,column = 2,padx = 5,pady = 5)
+enter = Button(inputFrame,text = 'Enter->',fg = 'black',bg = '#99ffff',height = 1,width = 10,font = ('Comic Sans MS',14),command = '' )
+enter.grid(row = 1,column = 2,padx = 5,pady = 5)
 
 sortSpeed = Scale(inputFrame,from_ = 1,to = 100,resolution = 0.1,length = 400,width = 15,orient = HORIZONTAL,label = 'Sorting Speed [s]',font = ('Comic Sans MS',10))
 sortSpeed.grid(row = 2,column = 0,padx = 5,pady = 5,columnspan = 2)
 
-# pause = Button(inputFrame,text = 'Pause',fg = 'black',bg = '#ff6600',height = 1,width = 10,font = ('Comic Sans MS',14),command = pauseSort )
-# pause.grid(row = 2,column = 2,padx = 5,pady = 5)
+play = Button(inputFrame,text = 'Play',fg = 'black',bg = '#00ff00',height = 1,width = 10,font = ('Comic Sans MS',14),command = startSort )
+play.grid(row = 2,column = 2,padx = 5,pady = 5)
+
+xxx = ScrolledText(inputFrame,width = 20,height = 12)
+xxx.grid(row = 0,column = 3,padx = 5,pady = 5,rowspan = 3)
 
 #--output frame------------------------------------------------------
     #---displayArray---
