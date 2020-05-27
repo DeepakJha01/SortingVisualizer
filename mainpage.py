86a2bd7683fd7732ee05aea1eaa52befc46b5cd5
Hugh Brown <hughdbrown@yahoo.com>
2020-05-27 15:51:37 -0600

    Remove dead code, do lookup in dict

--- mainpage.py
+++ mainpage.py
@@ -3,13 +3,15 @@
 import time
 import math
 import random
+
 from tkinter import *
 from tkinter import ttk
+
 from mergesort import mergeSort
 from quicksort import quickSort
 from bubblesort import bubbleSort
 from selectionsort import selectionSort
-# from tkinter.scrolledtext import ScrolledText
+
 #---end import section---------------------
 
 
@@ -34,22 +36,15 @@ def generateRandomArray():
     arr = list(range(1, n + 1))
     random.shuffle(arr)
 
-    #random shuffle
-    for i in range(n-1,0,-1):
-        ind = math.floor(random.random()*(i+1))
-        arr[i],arr[ind] = arr[ind],arr[i]
-
-    arrayColor = ['red' for i in range(n)]
+    arrayColor = ['red']  * n
 
     swapCount = 0
     displayArray(arr,arrayColor,swapCount)
-    # return arr
 
 def normalizeArray(arr):
-    return [i/max(arr) for i in arr]
+    return [i / max(arr) for i in arr]
 
 def displayArray(arr,arrayColor,opCount):
-
     outputCanvas.delete('all')
     n = len(arr)
 
@@ -72,35 +67,25 @@ def displayArray(arr,arrayColor,opCount):
 
         outputCanvas.create_rectangle(x0,y0,x1,y1, fill = arrayColor[i])
 
-    ##display swapCount
     swapCountLabel = Label(outputCanvas,text = '#Swap Count : '+str(opCount),fg = 'white',bg = 'black',font = ('Comic Sans MS',12))
     outputCanvas.create_window(80,20,window = swapCountLabel)
 
     root.update_idletasks()
 
+# Map from string to sorting function
+lookup = {
+    'Bubble Sort': bubbleSort,
+    'Selection Sort': selectionSort,
+    'Merge Sort': mergeSort,
+    'Quick Sort': quickSort,
+}
+
+
 def startSort():
     global arr
+    fn = lookup[algoCombo.get()]
+    fn(arr,displayArray,sortSpeed.get(),pauseBool)
 
-    if algoCombo.get() == 'Bubble Sort':
-        bubbleSort(arr,displayArray,sortSpeed.get(),pauseBool)
-
-    elif algoCombo.get() == 'Selection Sort':
-        selectionSort(arr,displayArray,sortSpeed.get(),pauseBool)
-
-    elif algoCombo.get() == 'Merge Sort':
-        mergeSort(arr,displayArray,sortSpeed.get(),pauseBool,0,len(arr)-1)
-
-    elif algoCombo.get() == 'Quick Sort':
-        quickSort(arr,displayArray,sortSpeed.get(),pauseBool,0,len(arr)-1)
-
-# def pauseSort():
-#     global pauseBool
-#     pauseBool = True
-    # return bubbleSort
-
-
-# arr = generateRandomArray(5)
-# displayArray(arr)
 
 #----User Interface Section---------------------------------------------------------------------------------------------
 inputFrame = Frame(root,height = 200,width = 950,bg = 'black')
@@ -123,19 +108,12 @@ generate.grid(row = 0,column = 2,padx = 5,pady = 5)
 dataSize = Scale(inputFrame,from_ = 3,to = 100,resolution = 1,length = 400,width = 15,orient = HORIZONTAL,label = 'Data Size [n]',font = ('Comic Sans MS',10))
 dataSize.grid(row = 1,column = 0,padx = 5,pady = 5,columnspan = 2)
 
-# enter = Button(inputFrame,text = 'Enter->',fg = 'black',bg = '#99ffff',height = 1,width = 10,font = ('Comic Sans MS',14),command = '' )
-# enter.grid(row = 1,column = 2,padx = 5,pady = 5)
-
 sortSpeed = Scale(inputFrame,from_ = 1,to = 100,resolution = 0.1,length = 400,width = 15,orient = HORIZONTAL,label = 'Sorting Speed [s]',font = ('Comic Sans MS',10))
 sortSpeed.grid(row = 2,column = 0,padx = 5,pady = 5,columnspan = 2)
 
 play = Button(inputFrame,text = 'Play',fg = 'black',bg = '#00ff00',height = 5,width = 10,font = ('Comic Sans MS',14),command = startSort )
 play.grid(row = 1,column = 2,padx = 5,pady = 5,rowspan = 2)
 
-# xxx = ScrolledText(inputFrame,width = 20,height = 12)
-# xxx.grid(row = 0,column = 3,padx = 5,pady = 5,rowspan = 3)
-
 #--output frame------------------------------------------------------
-    #---displayArray---
 
 root.mainloop()
