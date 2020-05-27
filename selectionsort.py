ae3bc9222b4273a2977cd6490dd4e6836a18f846
Hugh Brown <hughdbrown@yahoo.com>
2020-05-27 16:43:46 -0600

    Fix colorArray for selection sort

--- selectionsort.py
+++ selectionsort.py
@@ -5,32 +5,29 @@ max_time = 0.250
 #finds the minimum value of the remaining array everytime
 def selectionSort(arr,displayArray,speedInput,pauseBool):
     swapCount = 0
-
-    for i in range(len(arr)):
+    N = len(arr)
+    for i in range(N):
         min_ind = i
-        for j in range(i+1,len(arr)):
+        for j in range(i + 1, N):
             if arr[j] < arr[min_ind]:
                 min_ind = j
 
-                colorArray = []
-                for x in range(len(arr)):
-                    if x<i:
-                        colorArray.append('green')
-                    elif x==j or x==min_ind:
-                        colorArray.append('blue')
-                    else:
-                        colorArray.append('red')
+                colorArray = ['red'] * N
+                colorArray[:i] = ['green'] * i
+                colorArray[j] = 'blue'
+                colorArray[min_ind] = 'blue'
 
                 displayArray(arr,colorArray,swapCount)
                 time.sleep(max_time - (speedInput * max_time / 100))
 
         arr[i],arr[min_ind] = arr[min_ind],arr[i]
         swapCount+=1
-        colorArray = ['green' if x<=i else 'red' for x in range(len(arr))]
+        # colorArray = ['green' if x<=i else 'red' for x in range(len(arr))]
+        colorArray = ['green'] * (i + 1) + ['red'] * (N - i - 1)
         displayArray(arr,colorArray,swapCount)
         time.sleep(max_time - (speedInput * max_time / 100))
 
-    colorArray = ['green' for i in range(len(arr))]
+    colorArray = ['green'] * N
     displayArray(arr, colorArray, swapCount)
     print("Sorted arr : ", arr)
 
